acb9ccfd8a2c5c3486a627eb5169a19369f0fe84
Victor Pontis <victor@pont.is>
2022-05-03 13:28:26 -0400

    Add test for transferring NFT (#21)

--- programs/nftoken/src/account_types.rs
+++ programs/nftoken/src/account_types.rs
@@ -22,18 +22,18 @@ pub struct CollectionAccount {
 pub struct NftAccount {
     // TODO: add version
 
-    pub holder: Pubkey,
+    pub holder: Pubkey, // 32
 
-    pub creator: Pubkey,
-    pub creator_can_update: bool,
+    pub creator: Pubkey, // 32
+    pub creator_can_update: bool, // 1
 
-    pub name: [u8; 32],
-    pub image_url: [u8; 128],
-    pub metadata_url: [u8; 128],
+    pub name: [u8; 32], // 32
+    pub image_url: [u8; 128], // 128
+    pub metadata_url: [u8; 128], // 128
 
-    pub collection: Pubkey,
-    pub delegate: Pubkey,
+    pub collection: Pubkey, // 32
+    pub delegate: Pubkey, // 32
 
-    pub created_at: i64,
+    pub created_at: i64, // 8
 }
 
