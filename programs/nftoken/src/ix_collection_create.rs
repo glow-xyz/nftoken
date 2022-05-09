ddb99b9d593171717cc0b41329501fed9bd6dd49
Victor Pontis <victor@pont.is>
2022-05-09 16:23:18 -0400

    Fixup

--- programs/nftoken/src/ix_collection_create.rs
+++ programs/nftoken/src/ix_collection_create.rs
@@ -9,12 +9,12 @@ pub fn collection_create_inner(
     ctx: Context<CollectionCreate>,
     args: CollectionCreateArgs,
 ) -> Result<()> {
-    let collection_account = &mut ctx.accounts.collection_account;
+    let collection = &mut ctx.accounts.collection;
 
-    collection_account.version = 1;
-    collection_account.creator = ctx.accounts.creator.key();
-    collection_account.metadata_url = args.metadata_url;
-    collection_account.creator_can_update = true;
+    collection.version = 1;
+    collection.creator = ctx.accounts.creator.key();
+    collection.metadata_url = args.metadata_url;
+    collection.creator_can_update = true;
 
     Ok(())
 }
@@ -26,7 +26,7 @@ pub struct CollectionCreate<'info> {
     pub creator: Signer<'info>,
 
     #[account(init, payer = creator, space = COLLECTION_ACCOUNT_SIZE)]
-    pub collection_account: Account<'info, CollectionAccount>,
+    pub collection: Account<'info, CollectionAccount>,
 
     pub system_program: Program<'info, System>,
 }
--- programs/nftoken/src/ix_nft_unset_collection.rs
+++ programs/nftoken/src/ix_nft_unset_collection.rs
@@ -1,7 +1,7 @@
-use anchor_lang::prelude::*;
-use crate::errors::*;
+use crate::account_types::NftAccount;
 use crate::constants::*;
-use crate::account_types::{NftAccount};
+use crate::errors::*;
+use anchor_lang::prelude::*;
 
 /// # Unset Collection
 ///
@@ -19,13 +19,13 @@ use crate::account_types::{NftAccount};
 /// In the future, we may want to allow the collection authority to remove NFTs from a collection
 /// without auth from the NFT creator.
 pub fn nft_unset_collection_inner(ctx: Context<NftUnsetCollection>) -> Result<()> {
-    let nft_account = &mut ctx.accounts.nft;
+    let nft = &mut ctx.accounts.nft;
     let nft_authority_key = ctx.accounts.nft_authority.key();
 
-    let has_nft_auth = nft_account.creator.key() == nft_authority_key && nft_account.creator_can_update;
+    let has_nft_auth = nft.creator.key() == nft_authority_key && nft.creator_can_update;
     require!(has_nft_auth, NftokenError::Unauthorized);
 
-    nft_account.collection = NULL_PUBKEY;
+    nft.collection = NULL_PUBKEY;
 
     Ok(())
 }
