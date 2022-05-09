cb5d31f28e8645f9aed970baaf9d47f42f6d6453
Victor Pontis <victor@pont.is>
2022-05-09 00:39:07 -0400

    Pull out mint info index into it's own func

--- programs/nftoken/src/ix_mintlist_mint_nft.rs
+++ programs/nftoken/src/ix_mintlist_mint_nft.rs
@@ -4,6 +4,7 @@ use crate::errors::*;
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program;
 use anchor_lang::solana_program::system_instruction;
+use std::convert::TryInto;
 
 /// # Mint NFT
 ///
@@ -13,6 +14,8 @@ pub fn mintlist_mint_nft_inner(ctx: Context<MintlistMintNft>) -> Result<()> {
     let current_time = ctx.accounts.clock.unix_timestamp;
     let signer = &ctx.accounts.signer;
     let treasury = &ctx.accounts.treasury_sol;
+
+    // TODO: take some money here to punish bots?
 
     // Require that the mintlist is configured and ready for minting.
     require!(
@@ -25,25 +28,27 @@ pub fn mintlist_mint_nft_inner(ctx: Context<MintlistMintNft>) -> Result<()> {
     let ix = system_instruction::transfer(&signer.key(), &treasury.key(), mintlist.price);
     solana_program::program::invoke(&ix, &[signer.to_account_info().clone(), treasury.clone()])?;
 
-    // Find the `mint_info` for the NFT we are going to mint
-    // TODO: support random generation mode
-    let mint_info_size = MintInfo::size();
-    let mint_info_pos = MintlistAccount::size(mintlist.num_nfts_redeemed);
-    mintlist.num_nfts_redeemed += 1; // TODO: do checked addition
     let mintlist_account_info = mintlist.to_account_info();
     let mut mintlist_data = mintlist_account_info.data.borrow_mut();
+
+    // Find the `mint_info` for the NFT we are going to mint
+    let mint_info_index = get_mint_info_index(&*mintlist);
+    let mint_info_pos = MintlistAccount::size(mint_info_index.try_into().unwrap());
 
     // We make sure that we haven't minted this yet.
     require!(
         mintlist_data[mint_info_pos] == 0,
         NftokenError::Unauthorized
     );
+
     // The first byte of the `mint_info` is `minted`, so this sets minted = true
     mintlist_data[mint_info_pos] = 1;
+
+    let mint_info_size = MintInfo::size();
     let mint_info_data = &mut &mintlist_data[mint_info_pos..(mint_info_pos + mint_info_size)];
     let mint_info: MintInfo = AnchorDeserialize::deserialize(mint_info_data)?;
 
-    // Configure the minted NFT!
+    // Configure the minted NFT
     let nft = &mut ctx.accounts.nft;
     nft.version = 1;
     nft.collection = mintlist.collection;
@@ -52,7 +57,14 @@ pub fn mintlist_mint_nft_inner(ctx: Context<MintlistMintNft>) -> Result<()> {
     nft.metadata_url = mint_info.metadata_url;
     nft.creator_can_update = true;
 
+    mintlist.num_nfts_redeemed = mintlist.num_nfts_redeemed.checked_add(1).unwrap();
+
     Ok(())
+}
+
+fn get_mint_info_index(mintlist: &Account<MintlistAccount>) -> usize {
+    // TODO: support random generation mode
+    return mintlist.num_nfts_redeemed.try_into().unwrap();
 }
 
 #[derive(Accounts)]
