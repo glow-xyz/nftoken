5a388007370bcb9ed7d5796eb7eeb383ff28538e
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-26 15:40:11 -0400

    Build out form

--- docs/components/CreateNftSection.tsx
+++ docs/components/CreateNftSection.tsx
@@ -1,8 +1,75 @@
+import { useState } from "react";
+import classNames from "classnames";
 
 export const CreateNftSection = () => {
+  const [imageinDropzone, setImageInDropzone] = useState(false);
+
   return (
     <div>
-      <b>Hi this is the create NFT section</b>
+      <form onSubmit={() => console.log("submit")}>
+        <div>
+          <label htmlFor="name" className="luma-input-label medium">
+            Name
+          </label>
+          <input type="text" name="name" id="name" className="luma-input" />
+        </div>
+
+        <div>
+          <label htmlFor="image" className="luma-input-label medium">
+            NFT Image
+          </label>
+          <div
+            className={classNames("dropzone", { active: imageinDropzone })}
+            onDragEnter={() => setImageInDropzone(true)}
+            onDragOver={() => setImageInDropzone(true)}
+            onDragLeave={() => setImageInDropzone(false)}
+            onDrop={() => setImageInDropzone(false)}
+          >
+            <input type="file" name="image" id="image" />
+            <div>Drag and drop an image or click to browse.</div>
+          </div>
+        </div>
+
+        <button className="luma-button round brand solid flex-center mt-4">
+          Create NFT
+        </button>
+      </form>
+
+      <style jsx>{`
+        form > div {
+          margin-bottom: 1rem;
+        }
+
+        .dropzone {
+          position: relative;
+          border: 1px dashed var(--primary-border-color);
+          width: max-content;
+          color: var(--secondary-color);
+          background-color: var(--secondary-bg-color);
+          padding: 1rem;
+          border-radius: var(--border-radius);
+          transition: var(--transition);
+        }
+
+        .dropzone.active {
+          color: var(--success-color);
+          border-color: var(--success-color);
+          background-color: var(--success-pale-bg-color);
+        }
+
+        .dropzone:hover {
+          background-color: var(--tertiary-bg-color);
+        }
+
+        input[type="file"] {
+          opacity: 0;
+          position: absolute;
+          inset: 0;
+          width: 100%;
+          height: 100%;
+          cursor: pointer;
+        }
+      `}</style>
     </div>
-  )
-}
+  );
+};
