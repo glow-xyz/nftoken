d766fcaca0c0937c3f078e91465a65806ee2c42e
Victor Pontis <victor@pont.is>
2022-05-31 14:59:01 +0200

    Update deploy script and program ID (#57)
    * Update deploy script and program ID
    
    * Add deploy:mainnet
    
    * Add deploy script

--- program-tests/utils/test-utils.ts
+++ program-tests/utils/test-utils.ts
@@ -14,7 +14,7 @@ export const program = anchor.workspace.Nftoken as Program<_NftokenIdlType>;
 
 export const NftokenIdl = _NftokenIdl;
 export type NftokenIdlType = _NftokenIdlType;
-export const PROGRAM_ID = "nf6WUrqMxWm2eQJ9m1H9BPFjKYyBVzue546URy4ULDC";
+export const PROGRAM_ID = "nf7SGC2ZAruzXwogZRffpATHwG8j7fJfxppSWaUjCfi";
 
 // Anchor uses `nodewallet.ts` when testing to find and use a wallet. It automatically signs
 // transactions with this keypair.
