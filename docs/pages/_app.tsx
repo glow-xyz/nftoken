fd5267efc7fd91e921fd8f4d47ecbe7b6a713ede
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-07 12:52:27 -0400

    wip

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,31 +1,17 @@
 import { GlowProvider } from "@glow-app/glow-react";
 import "@glow-app/glow-react/dist/styles.css";
+
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { SideNav } from "../components/shell/SideNav";
-import { TopNav } from "../components/shell/TopNav";
+import Link from "next/link";
+import classNames from "classnames";
 
 import "../public/globals.css";
 import "../styles/app.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [navOpen, setNavOpen] = useState(false);
-
-  useEffect(() => {
-    if (navOpen) {
-      document.body.classList.add("no-scroll");
-    } else {
-      document.body.classList.remove("no-scroll");
-    }
-  }, [navOpen]);
-
   const router = useRouter();
-
-  useEffect(() => {
-    setNavOpen(false);
-  }, [router.pathname]);
 
   return (
     <GlowProvider>
@@ -34,78 +20,137 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <TopNav
-        navOpen={navOpen}
-        toggleNav={() => {
-          setNavOpen((open) => !open);
-        }}
-      />
+      <div className="wrapper">
+        <header className="spread">
+          <Link href="/">
+            <a className="logo">NFToken</a>
+          </Link>
+          <a
+            href="https://github.com/glow-xyz/nftoken"
+            target="_blank"
+            className="github"
+          >
+            <span>GitHub</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+            </svg>
+          </a>
+        </header>
 
-      <main>
-        <SideNav />
-        <Component {...pageProps} />
-      </main>
+        <div className="content">
+          <nav>
+            <div className="nav-inner">
+              {nav.map((item) => (
+                <Link href={item.href} key={item.title}>
+                  <a
+                    className={classNames({
+                      current: router.pathname === item.href,
+                    })}
+                  >
+                    {item.title}
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </nav>
+
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </div>
 
       <style jsx>{`
-        main {
+        .wrapper {
+          min-height: 100vh;
           display: grid;
-          grid-template-columns: 16rem 65ch 1fr;
-          grid-column-gap: 4rem;
+          grid-template-rows: max-content 1fr;
         }
 
-        main > :global(*):not(article) {
+        header {
+          padding: 1.2rem 3rem;
+          border-top: 4px solid var(--brand-color);
+          border-bottom: 1px solid var(--secondary-border-color);
           position: sticky;
-          top: var(--top-nav-height);
-          height: calc(100vh - var(--top-nav-height));
+          top: 0;
+          background-color: var(--primary-bg-color);
+          z-index: 100;
         }
 
-        main > :global(*):nth-child(1) {
-          padding: 2rem 0 2rem 2rem;
+        .logo {
+          display: block;
+          font-weight: 900;
+          color: var(--primary-color);
+          margin: 0;
+          font-size: 1.1rem;
         }
 
-        main > :global(*):nth-child(2) {
-          padding-top: 2rem;
-          padding-bottom: 5rem;
+        .github {
+          display: block;
+          background-color: var(--brand-color);
+          color: var(--white);
+          font-size: var(--small-font-size);
+          font-weight: 600;
+          height: max-content;
+          padding: 0.2rem 0.8rem;
+          border-radius: 99px;
         }
 
-        main > :global(*):nth-child(3) {
-          padding: 2rem 2rem 2rem 0;
+        .github svg {
+          height: 1rem;
+          width: 1rem;
+          margin-left: 0.3rem;
+          transform: translateY(-0.1rem);
         }
 
-        @media (max-width: 1300px) {
-          main {
-            grid-template-columns: 12rem 55ch 1fr;
-            grid-column-gap: 2rem;
-          }
+        .content {
+          display: grid;
+          grid-template-columns: 320px 1fr;
+          height: 100%;
         }
 
-        @media (max-width: 1000px) {
-          main > :global(*):nth-child(3) {
-            display: none;
-          }
+        nav {
+          border-right: 1px solid var(--secondary-border-color);
+          height: 100%;
         }
 
-        @media (max-width: 800px) {
-          main > :global(*):nth-child(1) {
-            padding: 1rem;
-            position: fixed;
-            inset: 0;
-            top: var(--top-nav-height);
-            background: var(--primary-bg-color);
-            display: ${navOpen ? "block" : "none"};
-          }
+        .nav-inner {
+          padding-left: 3rem;
+          position: sticky;
+          top: 6rem;
+        }
 
-          main {
-            display: block;
-          }
+        nav a {
+          color: var(--secondary-color);
+          display: block;
+          margin-bottom: 0.3rem;
+          transition: none;
+          max-width: max-content;
+        }
 
-          main > :global(*):nth-child(2) {
-            padding-top: 1rem;
-            padding-left: 1rem;
-            padding-right: 1rem;
-          }
+        nav a.current {
+          color: var(--brand-color);
+          font-weight: 600;
+          text-decoration: underline;
+        }
+
+        main {
+          padding: 3rem 4rem;
+          max-width: 50rem;
         }
       `}</style>
     </GlowProvider>
   );
 }
+
+const nav = [
+  { title: "Overview", href: "/" },
+  { title: "Installation", href: "/installation" },
+  { title: "Usage", href: "/usage" },
+];
