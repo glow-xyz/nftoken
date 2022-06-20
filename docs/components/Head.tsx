ac2f3895e0886d2363730555c9d5e422fe0bd8e4
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-20 13:39:32 -0400

    Add og:title and twitter:title to try to make cards-dev.twitter.com happy

--- docs/components/Head.tsx
+++ docs/components/Head.tsx
@@ -5,8 +5,12 @@ export const Head = ({ title }: { title: string }) => {
     <NextHead>
       <title>{title}</title>
       <link rel="icon" href="/favicon.png" />
+
+      <meta property="og:title" content={title} />
       <meta property="og:image" content="https://nftoken.so/share.png" />
+
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
     </NextHead>
   );
 };
