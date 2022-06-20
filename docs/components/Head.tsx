a5242924dcdc67bf98f013b5ebc383df14c089be
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-20 13:37:17 -0400

    Wrap with next/head

--- docs/components/Head.tsx
+++ docs/components/Head.tsx
@@ -1,10 +1,12 @@
+import NextHead from "next/head";
+
 export const Head = ({ title }: { title: string }) => {
   return (
-    <>
+    <NextHead>
       <title>{title}</title>
       <link rel="icon" href="/favicon.png" />
       <meta property="og:image" content="https://nftoken.so/share.png" />
       <meta name="twitter:card" content="summary_large_image" />
-    </>
+    </NextHead>
   );
 };
