c57734fd91335fa760f139304b39a8a343b04f54
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-23 15:25:28 -0400

    Copy over new modal

--- docs/components/LuxModal.tsx
+++ docs/components/LuxModal.tsx
@@ -10,9 +10,8 @@ import {
   FRAMER_EASE,
   FRAMER_TRANSITION,
 } from "../utils/framer";
-import { ResponsiveBreakpoint } from "../utils/style-constants";
 
-type ModalVariant = "regular" | "compact" | "lu-alert" | "sticky-footer";
+type ModalVariant = "regular" | "compact" | "lux-alert" | "sticky-footer";
 type RenderProps =
   | {
       children: null | React.ReactNode;
@@ -95,6 +94,7 @@ export const LuxModalContainer = ({
     return () => {
       document.removeEventListener("keydown", handleEscape);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleEscape = (event: KeyboardEvent) => {
@@ -125,14 +125,6 @@ export const LuxModalContainer = ({
           </AnimatePresence>,
           document.body
         )}
-
-      <style jsx>{`
-        :global(#__next) {
-          // Create a stacking context on next so that the modal is in front of everything
-          position: relative;
-          z-index: 1;
-        }
-      `}</style>
     </>
   );
 };
@@ -145,47 +137,14 @@ const LuxModalHeader = ({
   onHide: () => void;
 }) => {
   return (
-    <div className="lu-modal-header flex-center spread bg-primary">
+    <div className="lux-modal-header flex-center spread bg-primary">
       <div className="title">{title}</div>
       <button
         className="btn close flex-center-center animated"
         onClick={onHide}
       >
-        <XIcon />
+        <CloseIcon />
       </button>
-
-      <style jsx>{`
-        .lu-modal-header {
-          padding: var(--modal-header-footer-padding);
-          border-bottom: 1px solid var(--divider-color);
-        }
-
-        .title {
-          color: var(--primary-color);
-          font-size: 1.1rem;
-          font-weight: var(--bold-font-weight);
-        }
-
-        .close {
-          padding: 0;
-          border: 0;
-          border-radius: 100px;
-          background-color: var(--secondary-color);
-          color: var(--modal-bg-color);
-          width: 1.25rem;
-          height: 1.25rem;
-
-          opacity: 0.75;
-
-          &:hover {
-            opacity: 1;
-          }
-        }
-
-        @media (max-width: ${ResponsiveBreakpoint.tiny}) {
-          background-color: var(--modal-header-bg-color);
-        }
-      `}</style>
     </div>
   );
 };
@@ -205,7 +164,7 @@ const LuxModal = ({
 } & RenderProps) => {
   return (
     <motion.div
-      className={classNames("lu-modal", className, variant)}
+      className={classNames("lux-modal", className, variant)}
       onClick={(e) => {
         e.stopPropagation();
       }}
@@ -220,79 +179,19 @@ const LuxModal = ({
       {title && <LuxModalHeader title={title} onHide={onHide} />}
 
       {children ? (
-        <div className="lu-modal-body overflow-auto">{children}</div>
+        <div className="lux-modal-body overflow-auto">{children}</div>
       ) : (
         render?.({
           Body: ({ children }) => (
-            <div className="lu-modal-body overflow-auto">{children}</div>
+            <div className="lux-modal-body overflow-auto">{children}</div>
           ),
           Footer: ({ children, className }) => (
-            <div className={classNames("lu-modal-footer", className)}>
+            <div className={classNames("lux-modal-footer", className)}>
               {children}
             </div>
           ),
         })
       )}
-
-      <style jsx global>{`
-        .lu-modal {
-          overflow: hidden;
-          display: flex;
-          flex-direction: column;
-
-          width: 480px;
-          max-height: 70vh;
-
-          border-radius: var(--modal-border-radius);
-          box-shadow: var(--shadow-lg);
-          background-color: var(--modal-bg-color);
-          border: var(--modal-border);
-
-          &.sticky-footer {
-            .lu-modal-body {
-              background-color: var(--secondary-bg-color);
-            }
-          }
-
-          &.lu-alert {
-            width: 360px;
-          }
-
-          @media (max-width: ${ResponsiveBreakpoint.small}) {
-            &:not(.lu-alert) {
-              width: calc(100% - 40px);
-              max-width: 480px;
-            }
-          }
-
-          @media (max-width: ${ResponsiveBreakpoint.tiny}) {
-            &:not(.compact):not(.lu-alert) {
-              width: 100%;
-              height: 100%;
-              border-radius: 0;
-              max-width: 100%;
-              max-height: 100%;
-            }
-
-            &.lu-alert {
-              width: calc(100% - 40px);
-              max-width: 360px;
-            }
-          }
-
-          .lu-modal-body {
-            width: 100%;
-            padding: var(--modal-padding);
-          }
-
-          .lu-modal-footer {
-            border-top: 1px solid var(--divider-color);
-            background-color: var(--primary-bg-color);
-            width: 100%;
-            padding: var(--modal-header-footer-padding);
-          }
-        }
-      `}</style>
     </motion.div>
   );
 };
@@ -314,7 +213,7 @@ const ModalOverlay = ({
 
   return (
     <motion.div
-      className={classNames("lu-modal-overlay", { pop })}
+      className={classNames("lux-modal-overlay", { pop })}
       onClick={canClickOutToDismiss ? onHide : onPop}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
@@ -325,39 +224,6 @@ const ModalOverlay = ({
       }}
     >
       {children}
-      <style jsx global>{`
-        .lu-modal-overlay {
-          position: fixed;
-          display: flex;
-          justify-content: center;
-          align-items: flex-start;
-          padding-top: 15vh;
-          width: 100%;
-          height: 100%;
-          top: 0;
-          left: 0;
-          overflow: hidden;
-          z-index: 900;
-
-          background: var(--overlay-color);
-
-          @keyframes pop {
-            50% {
-              transform-origin: center;
-              transform: scale(1.005);
-            }
-          }
-
-          &.pop .lu-modal {
-            animation: pop 0.3s cubic-bezier(0.4, 0, 0.2, 1) 1;
-          }
-
-          @media (max-width: ${ResponsiveBreakpoint.tiny}) {
-            padding-top: 0;
-            align-items: center;
-          }
-        }
-      `}</style>
     </motion.div>
   );
 };
