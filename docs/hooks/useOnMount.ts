32be1cf688913fc27c82c9cb2b1d27577c0d7146
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-24 14:51:29 -0400

    lint

--- docs/hooks/useOnMount.ts
+++ docs/hooks/useOnMount.ts
@@ -3,6 +3,5 @@ import { EffectCallback, useEffect } from "react";
 export const useOnMount = (callback: EffectCallback) => {
   useEffect(() => {
     return callback();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
