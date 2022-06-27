e51298691416d2a66f4c4fd794c577780a73d00d
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-27 14:12:43 -0400

    Style NFT detail page

--- docs/pages/nft/[nftAddress].tsx
+++ docs/pages/nft/[nftAddress].tsx
@@ -6,6 +6,7 @@ import useSWR, { SWRResponse } from "swr";
 import { NftokenFetcher } from "../../utils/NftokenFetcher";
 import { NftokenTypes } from "../../utils/NftokenTypes";
 import { SolanaAddress } from "../../components/SolanaAddress";
+import React from "react";
 
 const useNft = ({
   nftAddress,
@@ -59,10 +60,34 @@ export default function NftPage({
         <img src={nft.image} />
         <div>
           <h1>{nft.name}</h1>
-          <SolanaAddress address={nft.address} />
-          <p>{nft.holder}</p>
-          <p>{nft.collection}</p>
-          <p>{JSON.stringify(nft.traits)}</p>
+          <div className="info">
+            <div className="info-box">
+              <p>Address</p>
+              <SolanaAddress address={nft.address} />
+            </div>
+            <div className="info-box">
+              <p>Holder</p>
+              <SolanaAddress address={nft.holder} />
+            </div>
+            <div className="info-box">
+              <p>Collection</p>
+              <SolanaAddress address={nft.collection} />
+            </div>
+          </div>
+
+          {nft.traits && (
+            <div className="trait-table">
+              {nft.traits.map(({ trait_type, value }, i) => (
+                <React.Fragment key={trait_type}>
+                  <p className="key">{trait_type}:</p>
+                  <p>{value}</p>
+                  {i !== nft.traits.length - 1 ? (
+                    <div className="divider" />
+                  ) : null}
+                </React.Fragment>
+              ))}
+            </div>
+          )}
         </div>
       </div>
 
@@ -77,10 +102,59 @@ export default function NftPage({
           box-shadow: var(--shadow-lg);
         }
 
+        h1 {
+          margin-bottom: 1.5rem;
+        }
+
         .columns {
           display: grid;
           grid-template-columns: max-content 1fr;
           grid-column-gap: 3rem;
+        }
+
+        .info-box {
+          display: inline-block;
+          margin-right: 1rem;
+          background-color: var(--secondary-bg-color);
+          padding: 0.5rem 0.75rem;
+          border-radius: var(--border-radius);
+        }
+
+        .info-box p {
+          margin-bottom: 0;
+          font-size: var(--tiny-font-size);
+          color: var(--secondary-color);
+          font-weight: var(--medium-font-weight);
+        }
+
+        .trait-table {
+          margin-top: 1.5rem;
+          display: grid;
+          grid-template-columns: max-content 1fr;
+          font-family: var(--mono-font);
+          background-color: var(--secondary-bg-color);
+          padding: 0.75rem;
+          border-radius: var(--border-radius);
+        }
+
+        .trait-table p {
+          margin-bottom: 0;
+          word-break: break-all;
+          font-size: var(--small-font-size);
+          font-weight: var(--medium-font-weight);
+        }
+
+        .trait-table .key {
+          padding-right: 1rem;
+          color: var(--secondary-color);
+          text-align: right;
+          font-weight: var(--normal-font-weight);
+        }
+
+        .trait-table .divider {
+          grid-column: span 2;
+          border-top: 1px solid var(--secondary-border-color);
+          margin: 0.5rem 0;
         }
       `}</style>
     </div>
