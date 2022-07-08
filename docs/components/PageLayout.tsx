f855ec92bf52ac8b9c1117db21133440d542ebd3
Ben Borgers <borgersbenjamin@gmail.com>
2022-07-08 15:32:10 -0400

    Allow for pages without secondaryNavLinks

--- docs/components/PageLayout.tsx
+++ docs/components/PageLayout.tsx
@@ -2,22 +2,29 @@ import { Header } from "./all-pages/Header";
 import { Footer } from "./all-pages/Footer";
 import { Toaster } from "react-hot-toast";
 import { SecondaryNav, SecondaryNavLink } from "./SecondaryNav";
+import classNames from "classnames";
 
 export const PageLayout = ({
   children,
   secondaryNavLinks,
 }: {
   children: React.ReactNode;
-  secondaryNavLinks: SecondaryNavLink[];
+  secondaryNavLinks?: SecondaryNavLink[];
 }) => {
   return (
     <>
       <div className="page">
         <Header />
-        <div className="content">
-          <div>
-            <SecondaryNav links={secondaryNavLinks} />
-          </div>
+        <div
+          className={classNames("content", {
+            "with-secondary-nav": secondaryNavLinks,
+          })}
+        >
+          {secondaryNavLinks && (
+            <div>
+              <SecondaryNav links={secondaryNavLinks} />
+            </div>
+          )}
           <div>{children}</div>
         </div>
         <Footer />
@@ -34,6 +41,11 @@ export const PageLayout = ({
           width: 100%;
           max-width: 60rem;
           margin: 0 auto;
+          padding: 1.5rem;
+          padding-bottom: 3rem;
+        }
+
+        .content.with-secondary-nav {
           padding-left: 0.75rem;
           padding-right: 1.5rem;
           padding-top: 2rem;
