3f29001c8802bbff597e37e3d60eee998c8cbfb8
Ben Borgers <borgersbenjamin@gmail.com>
2022-07-08 16:32:08 -0400

    lint

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -3,18 +3,13 @@ import "@glow-app/glow-react/dist/styles.css";
 
 import { NetworkProvider } from "../components/NetworkContext";
 
-import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import { Header } from "../components/all-pages/Header";
 import { NextPreviousButtons } from "../components/all-pages/NextPreviousButtons";
-import { TabBar } from "../components/all-pages/TabBar";
-import { Footer } from "../components/all-pages/Footer";
 import { SocialHead } from "../components/SocialHead";
 import "../public/globals.css";
 import "../styles/app.scss";
 
-import { ResponsiveBreakpoint } from "../utils/style-constants";
 import { PageLayout } from "../components/PageLayout";
 import { DOC_PAGES } from "../components/all-pages/navigation-constants";
 
