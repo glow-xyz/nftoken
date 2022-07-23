ece17f4a6d6df1288a4a47cd6a551de4a62ac330
vlawmz <flawmz@gmail.com>
2022-07-22 21:53:32 -0600

    Check is_frozen in burn instruction

--- programs/nftoken/src/ix_nft_burn.rs
+++ programs/nftoken/src/ix_nft_burn.rs
@@ -1,10 +1,13 @@
 use crate::account_types::*;
 use anchor_lang::prelude::*;
+use crate::errors::*;
 
 /// # Burn NFT
 ///
 /// Burning an NFT closes the account and returns the SOL to the current holder.
-pub fn nft_burn_inner(_ctx: Context<NftBurn>) -> Result<()> {
+pub fn nft_burn_inner(ctx: Context<NftBurn>) -> Result<()> {
+    require!(!ctx.accounts.nft.is_frozen, NftokenError::Unauthorized);
+
     Ok(())
 }
 
